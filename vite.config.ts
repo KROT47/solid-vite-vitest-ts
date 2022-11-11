9036345671a420cbc922d500dce61e8500eae481
Dmitry Leleko <lelekodmitry@gmail.com>
2022-11-12 00:22:42 +0300

    First commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import solid from "solid-start/vite";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    transformMode: {
+      web: [/\.[jt]sx?$/],
+    },
+    setupFiles: './setupVitest.ts',
+    // if you have few tests, try commenting one
+    // or both out to improve performance:
+    // threads: false,
+    // isolate: false,
+  },
+  plugins: [solid()],
+  resolve: {
+    conditions: ['development', 'browser'],
+  },
+});
