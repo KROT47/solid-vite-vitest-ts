d854cf84e1159853f3da5a4bcc1c798db702b166
Dmitry Leleko <dmitry.leleko@orionprotocol.io>
2022-11-23 23:16:20 +0300

    feat: Add modular-forms

--- src/entities/auth/ui/AuthForm.tsx
+++ src/entities/auth/ui/AuthForm.tsx
@@ -1,20 +1,22 @@
 import { createSignal } from 'solid-js';
 
 import { Box, Button, FormControl, FormControlLabel, Input } from '~/shared/ui';
+import MF, { Field, Form } from '~/shared/modular-forms';
 import { supabase } from '~/shared/supabase';
 
+type AuthForm = {
+  email: string;
+};
+
 export function AuthForm(): JSXElement {
+  const authForm = MF.createForm<AuthForm>();
+
   const [isLoading, setIsLoading] = createSignal(false);
-  const [email, setEmail] = createSignal('');
 
-  function handleEmailInput(e): void {
-    setEmail(e.target.value);
-  }
-
-  const handleLogin = async (): Promise<void> => {
+  const handleLogin = async ({ email }: AuthForm): Promise<void> => {
     try {
       setIsLoading(true);
-      const { error } = await supabase.auth.signInWithOtp({ email: email() });
+      const { error } = await supabase.auth.signInWithOtp({ email });
       if (error) throw error;
       alert('Check your email for the login link!');
     } catch (error) {
@@ -28,19 +30,27 @@ export function AuthForm(): JSXElement {
 
   return (
     <Box>
-      <FormControl>
-        <FormControlLabel for="email">Email address</FormControlLabel>
-        <Input
-          id="email"
-          type="email"
-          value={email()}
-          isRequired
-          onInput={handleEmailInput}
-        />
-      </FormControl>
-      <Button isLoading={isLoading()} variant="solid" onClick={handleLogin}>
-        Submit
-      </Button>
+      <Form of={authForm} onSubmit={handleLogin}>
+        <Field
+          name="email"
+          of={authForm}
+          validate={[
+            MF.required('Please enter your email.'),
+            MF.email('The email address is badly formatted.'),
+          ]}
+        >
+          {(field): JSXElement => (
+            <FormControl>
+              <FormControlLabel for="email">Email address</FormControlLabel>
+              <Input id="email" type="email" {...field.props} />
+              {field.error && <div>{field.error}</div>}
+            </FormControl>
+          )}
+        </Field>
+        <Button isLoading={isLoading()} type="submit" variant="solid">
+          Submit
+        </Button>
+      </Form>
     </Box>
   );
 }
---
+++ src/shared/modular-forms/index.ts
@@ -0,0 +1,6 @@
+import * as MF from '@modular-forms/solid';
+
+const { Form, Field } = MF;
+
+export { Form, Field };
+export default MF;
