1c9dcea66bbbf732870f76496eb513ff12e77ae7
Dmitry Leleko <dmitry.leleko@orionprotocol.io>
2022-11-23 17:59:00 +0300

    refactor: remove pages

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,3 +1,19 @@
-import { Page404 } from '~/pages';
+import { Title } from 'solid-start';
+import { HttpStatusCode } from 'solid-start/server';
 
-export default Page404;
+export default function Page404(): JSXElement {
+  return (
+    <main>
+      <Title>Not Found</Title>
+      <HttpStatusCode code={404} />
+      <h1>Page Not Found</h1>
+      <p>
+        Visit{' '}
+        <a href="https://start.solidjs.com" target="_blank">
+          start.solidjs.com
+        </a>{' '}
+        to learn how to build SolidStart apps.
+      </p>
+    </main>
+  );
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,20 @@
-import { HomePage } from '~/pages';
+import { Title } from 'solid-start';
 
-export default HomePage;
+import { Counter } from '~/entities/counter';
+
+export default function HomePage(): JSXElement {
+  return (
+    <main>
+      <Title>Hello World</Title>
+      <h1>Hello world!</h1>
+      <Counter />
+      <p>
+        Visit{' '}
+        <a href="https://start.solidjs.com" target="_blank">
+          start.solidjs.com
+        </a>{' '}
+        to learn how to build SolidStart apps.
+      </p>
+    </main>
+  );
+}
