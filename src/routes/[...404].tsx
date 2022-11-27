6fe1fd33a96726a3d6e218739d09609566345153
Dmitry Leleko <lelekodmitry@gmail.com>
2022-11-27 21:11:16 +0300

    Revert "refactor: remove pages"
    This reverts commit 1c9dcea66bbbf732870f76496eb513ff12e77ae7.

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,19 +1,3 @@
-import { Title } from 'solid-start';
-import { HttpStatusCode } from 'solid-start/server';
+import { Page404 } from '~/pages';
 
-export default function Page404(): JSXElement {
-  return (
-    <main>
-      <Title>Not Found</Title>
-      <HttpStatusCode code={404} />
-      <h1>Page Not Found</h1>
-      <p>
-        Visit{' '}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{' '}
-        to learn how to build SolidStart apps.
-      </p>
-    </main>
-  );
-}
+export default Page404;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,3 @@
-import { Title } from 'solid-start';
+import { HomePage } from '~/pages';
 
-import { Counter } from '~/entities/counter';
-
-export default function HomePage(): JSXElement {
-  return (
-    <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{' '}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{' '}
-        to learn how to build SolidStart apps.
-      </p>
-    </main>
-  );
-}
+export default HomePage;
