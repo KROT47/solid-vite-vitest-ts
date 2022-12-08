16d2757a3c20d7782086abe83b5e854eb5a5aff4
Dmitry Leleko <dmitry.leleko@orionprotocol.io>
2022-12-08 23:24:23 +0300

    chore: fixup

--- src/entities/Counter/Counter.model.ts
+++
@@ -1,8 +0,0 @@
-import { createApi, createStore } from 'effector';
-
-export const $counterState = createStore<number>(0);
-
-export const counterApi = createApi($counterState, {
-  inc: (state) => state + 1,
-  dec: (state) => state - 1,
-});
